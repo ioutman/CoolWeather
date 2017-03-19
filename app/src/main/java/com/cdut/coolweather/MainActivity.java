f08264afe8922696143f205a66cd39c6874faaf1
Mr.Li <540679928@qq.com>
2017-03-19 20:44:25 +0800

    检查网络可用

--- app/src/main/java/com/cdut/coolweather/MainActivity.java
+++ app/src/main/java/com/cdut/coolweather/MainActivity.java
@@ -1,10 +1,14 @@
 package com.cdut.coolweather;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this,WeatherActivity.class);
             startActivity(intent);
             finish();
+        }
+        checkNetWork();
+    }
+
+    private void checkNetWork() {
+        ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivity.getActiveNetworkInfo();
+        if ( info!= null && info.isAvailable()) {
+            //正常上网状况
+        } else {
+            Toast.makeText(this,"亲,你没有联网哦!",Toast.LENGTH_LONG).show();
         }
     }
 }
